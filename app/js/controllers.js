549fe91b29931e9021580841dac5b6e53de7c238
raskenne <kennylenana@yahoo.com>
2016-02-08 22:32:23 -0500

    more improvements

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,11 @@
 
 var bookstoreApp = angular.module('bookstoreApp', []);
 
-bookstoreApp.controller('BookListCtrl',function ($scope){
-	$scope.books = [
-	     {'name': 'Nexus S',
-	     'snippet': 'Fast just got faster with Nexus S.'},
-	     {'name': 'Motorola XOOM™ with Wi-Fi',
-	      'snippet': 'The Next, Next Generation tablet.'},
-	     {'name': 'MOTOROLA XOOM™',
-	      'snippet': 'The Next, Next Generation tablet.'}
-	      ];
-});
-	
-	
+bookstoreApp.controller('BookListCtrl' , ['$scope' , '$http' , function($scope, $http){
+	$http.get('books/books.json').success(function(data){
+		$scope.books = data;
+		console.log(data)
+	});	
+}]);
+
+
